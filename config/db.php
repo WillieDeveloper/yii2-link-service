3f3f18b5c47444ada92ab686802cafb9188de6ce
WillieDeveloper <aristarch1hp@yandex.ru>
2025-06-04 21:18:18 +0300

    Initial commit Modify db connection

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_link_service',
+    'username' => 'user',
+    'password' => '1q2w3e4r5t',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
